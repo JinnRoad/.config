11a1cf11f65e831360120a1e321f27905c8af1ba
JinnRoad <JinnRoad@gmail.com>
2022-06-14 14:16:27 -0400

    Change bash prompt

--- windows-setup/clone-repos.ps1
+++ windows-setup/clone-repos.ps1
@@ -4,3 +4,4 @@ git clone git@github.com:jinnroad/learn-git
 git clone git@github.com:jinnroad/learn-linux
 git clone git@github.com:jinnroad/assessment
 git clone git@github.com:jinnroad/JinnRoad/cis117
+git clone git@github.com:jinnroad/JinnRoad/current
