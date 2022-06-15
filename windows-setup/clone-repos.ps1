3f03f48f5cd34537acaa8fc3c2af1b1640bf089a
JinnRoad <JinnRoad@gmail.com>
2022-06-15 14:12:48 -0400

    Change clone repos list

--- windows-setup/clone-repos.ps1
+++ windows-setup/clone-repos.ps1
@@ -3,5 +3,5 @@ git clone git@github.com:jinnroad/json2xml-dc
 git clone git@github.com:jinnroad/learn-git
 git clone git@github.com:jinnroad/learn-linux
 git clone git@github.com:jinnroad/assessment
-git clone git@github.com:jinnroad/JinnRoad/cis117
-git clone git@github.com:jinnroad/JinnRoad/current
+git clone git@github.com:jinnroad/cis117
+git clone git@github.com:jinnroad/current
