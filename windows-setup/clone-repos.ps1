1f724a11eb854b709ede67ff94ddab4f960bb4eb
JinnRoad <JinnRoad@gmail.com>
2022-06-29 13:45:01 -0400

    Change default repo list

--- windows-setup/clone-repos.ps1
+++ windows-setup/clone-repos.ps1
@@ -1,7 +1,7 @@
 cd ~/docs
-git clone git@github.com:jinnroad/json2xml-dc
 git clone git@github.com:jinnroad/learn-git
 git clone git@github.com:jinnroad/learn-linux
-git clone git@github.com:jinnroad/assessment
 git clone git@github.com:jinnroad/cis117
 git clone git@github.com:jinnroad/current
+git clone git@github.com:jinnroad/texts
+git clone git@github.com:jinnroad/notes
